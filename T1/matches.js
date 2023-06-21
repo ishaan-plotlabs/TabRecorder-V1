c4fcae9e8b412bd4edf3b5f0fb9192f428c87dd4
ishaan-plotlabs <137278987+ishaan-plotlabs@users.noreply.github.com>
2023-06-21 15:17:31 +0530

    Add files via upload

---
+++ T1/matches.js
@@ -0,0 +1,57 @@
+﻿let recorder = null;
+let streamId;
+let chunks;
+
+const start = (streamId) => {
+  navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: "tab",
+        chromeMediaSourceId: streamId
+      }
+    }
+  })
+    .then((stream) => {
+      console.log(stream);
+      recorder = new MediaRecorder(stream);
+      chunks = [];
+
+      recorder.ondataavailable = (e) => {
+        chunks.push(e.data);
+      };
+
+      recorder.onstop = (e) => {
+        running = false;
+        const blob = new Blob(chunks, { type: "video/webm;codecs=vp9" });
+        url = URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.download = "download.webm";
+        a.href = url;
+        a.click();
+      }
+
+      recorder.start();
+    });
+}
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log(message);
+  switch (message.command) {
+    case "start":
+      if (message.streamId != "") {
+        streamId = message.streamId;
+      }
+      if (recorder == null) {
+        start(streamId);
+      }
+      else if (recorder.state == "inactive") {
+        chunks = [];
+        recorder.start();
+      }
+      break;
+    case "stop":
+      recorder.stop();
+      break;
+  }
+});
---
+++ T1/popup.js
@@ -0,0 +1,20 @@
+﻿let tabId;
+
+chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  tabId = tabs[0].id;
+
+  chrome.tabCapture.getCapturedTabs((ci) => {
+    if (!ci.some(e => e.tabId == tabId && e.status == "active")) {
+      chrome.tabCapture.getMediaStreamId({ consumerTabId: tabId }, (streamId) => {
+        chrome.tabs.sendMessage(tabId, { command: "start", streamId: streamId });
+      });
+    }
+    else {
+      chrome.tabs.sendMessage(tabId, { command: "start", streamId: "" });
+    }
+  });
+});
+
+document.getElementById("stop").onclick = () => {
+  chrome.tabs.sendMessage(tabId, { command: "stop" });
+}
